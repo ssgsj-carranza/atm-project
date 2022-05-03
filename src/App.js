015309a3674ce3133abe84417d0f465ac0b3eca9
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-03 10:49:21 -0500

    created components folder, working on header for atm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <h1 className="title has-text-centered is-size-1">Its an ATM</h1>
+      <hr />
     </div>
   );
 }
